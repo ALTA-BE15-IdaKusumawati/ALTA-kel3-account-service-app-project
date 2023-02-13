913b1faa06acdc9203b424f2419eeca1d11c6fa2
kocwng <idakusuma2705@gmail.com>
2023-02-13 15:59:19 +0700

    update file main.go

---
+++ go.mod
@@ -0,0 +1,5 @@
+module account-service-app-project
+
+go 1.19
+
+require github.com/go-sql-driver/mysql v1.7.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
 func main() {
-	
+	var connectionString = os.Getenv("DB_CONNECTION")
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		//return nil, err
+		log.Fatal("error open connection", err.Error())
+	}
+	// See "Important settings" section.
+	db.SetConnMaxIdleTime(time.Minute * 3)
+	db.SetMaxOpenConns(10)
+	db.SetMaxIdleConns(10)
+
+	errPing := db.Ping()
+	if errPing != nil {
+		log.Fatal("error connect to db", errPing.Error())
+	} else {
+		fmt.Println("berhasil")
+	}
 }

e1da229391c00ccfdb813282f589dbde480b596a
kocwng <idakusuma2705@gmail.com>
2023-02-13 21:13:58 +0700

    update ERD

--- main.go
+++ main.go
@@ -1,31 +1,30 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
-	"os"
-	"time"
-
+	"account-service-app-project/config"
 	_ "github.com/go-sql-driver/mysql"
 )
 
 func main() {
-	var connectionString = os.Getenv("DB_CONNECTION")
-	db, err := sql.Open("mysql", connectionString)
-	if err != nil {
-		//return nil, err
-		log.Fatal("error open connection", err.Error())
-	}
-	// See "Important settings" section.
-	db.SetConnMaxIdleTime(time.Minute * 3)
-	db.SetMaxOpenConns(10)
-	db.SetMaxIdleConns(10)
+	// var connectionString = os.Getenv("DB_CONNECTION")
+	// // var connectionString = "root:sgkp8ghd%@tcp(127.0.0.1:3306)/db_project1"
+	// db, err := sql.Open("mysql", connectionString)
+	// if err != nil {
+	// 	//return nil, err
+	// 	log.Fatal("error open connection", err.Error())
+	// }
+	// // See "Important settings" section.
+	// db.SetConnMaxIdleTime(time.Minute * 3)
+	// db.SetMaxOpenConns(10)
+	// db.SetMaxIdleConns(10)
 
-	errPing := db.Ping()
-	if errPing != nil {
-		log.Fatal("error connect to db", errPing.Error())
-	} else {
-		fmt.Println("berhasil")
-	}
+	// errPing := db.Ping()
+	// if errPing != nil {
+	// 	log.Fatal("error connect to db", errPing.Error())
+	// } else {
+	// 	fmt.Println("berhasil")
+	// }
+	db := config.ConnectToDB()
+	defer db.Close()
+
 }

3e46c9fd6f2db0fe42c480f8fe72a7afe5d45bf8
1makki <yazkibay2@gmail.com>
2023-02-14 10:31:33 +0700

    add register and entities

--- db_project1.sql
+++ db_project1.sql
@@ -1,7 +1,38 @@
-<<<<<<< HEAD
 create database db_project1;
 
 use db_project1;
-=======
-CREATE DATABASE db_project1;
->>>>>>> e1da229391c00ccfdb813282f589dbde480b596a
+
+create table users(
+id varchar(50) primary key not null,
+name varchar(50),
+telepon int,
+email varchar(50),
+password varchar(25)
+);
+
+create table transfer(
+user_id_pengirim varchar(50),
+user_id_penerima varchar(50),
+nominal decimal,
+created_at datetime default current_timestamp,
+constraint fk_user_id_pengirim foreign key (user_id_pengirim) 
+references users(id),
+constraint fk_user_id_penerima foreign key (user_id_penerima) 
+references users(id)
+);
+
+create table topup(
+user_id varchar(50),
+nominal decimal,
+created_at datetime default current_timestamp,
+constraint fk_user_id foreign key (user_id) 
+references users(id)
+);
+
+create table saldo(
+user_id varchar(50),
+nominal decimal,
+constraint fk_user_id_saldo foreign key (user_id) 
+references users(id)
+);
+

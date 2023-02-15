5e9e2fe436406c681a82a616e36ecfa7d6eaa6d2
kocwng <idakusuma2705@gmail.com>
2023-02-15 13:03:55 +0700

    add fitur topup

---
+++ feature/topup.go
@@ -0,0 +1,48 @@
+package feature
+
+import (
+	"account-service-app-project/entities"
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+func EntryTopup(db *sql.DB, id string, duit entities.Topup) {
+
+	var query = "INSERT INTO topup(user_id, nominal) VALUES(?,?)"
+	statement, errPrepare := db.Prepare(query)
+	if errPrepare != nil {
+		log.Fatal("error", errPrepare.Error())
+	}
+
+	res, errInsert := statement.Exec(id, duit.Nominal)
+	if errInsert != nil {
+		log.Fatal("error", errInsert.Error())
+	} else {
+		ro, _ := res.RowsAffected()
+		if ro > 0 {
+			fmt.Println("Topup telah berhasil")
+		} else {
+			fmt.Println("Topup gagal")
+		}
+	}
+
+	ya, err := db.Prepare("UPDATE users INNER JOIN topup ON users.id=? SET saldo=saldo+?")
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+	// var n string = duit.User_ID  
+	res, errUpdate := ya.Exec(id, duit.Nominal)
+	if errUpdate != nil {
+		panic(errUpdate.Error())
+	}
+
+	a, errUpdate := res.RowsAffected()
+	if errUpdate != nil {
+		panic(errUpdate.Error())
+	}
+	if a > 0 {
+		fmt.Printf("Selamat, saldo anda telah bertambah")
+	}
+	
+}

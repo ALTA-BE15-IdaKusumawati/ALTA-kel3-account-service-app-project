ed9e7ab5eb48c7b984af55ebfa2f69a5a0de32b4
1makki <yazkibay2@gmail.com>
2023-02-16 21:19:53 +0700

    add feature updatesaldo

---
+++ feature/updatesaldo.go
@@ -0,0 +1,27 @@
+package feature
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func updateSaldo(db *sql.DB, nominal float32, id_penerima string) {
+	stmt, err := db.Prepare("UPDATE users SET saldo=saldo+? WHERE users.id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	result, err := stmt.Exec(nominal, id_penerima)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	RowsAffected, err := result.RowsAffected()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	if RowsAffected > 0 {
+		fmt.Println("Saldo updated")
+	}
+}

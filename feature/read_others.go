4f07344b577c57cb5110adbe77daaad9db474890
kocwng <idakusuma2705@gmail.com>
2023-02-16 15:10:38 +0700

    add fitur read other user

---
+++ feature/read_others.go
@@ -0,0 +1,33 @@
+package feature
+
+import (
+	"account-service-app-project/entities"
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+// func OtherUser(db *sql.DB, id string, profile entities.Users ) {
+func OtherUser(db *sql.DB, id string, profile entities.Users) {
+	ro, err := db.Query("SELECT telepon, name FROM users where telepon=?", profile.Telepon)
+	if err != nil {
+		log.Fatal("error select", err.Error())
+	}
+
+	var User []entities.Users
+	for ro.Next() {
+		var datarow entities.Users
+		errScan := ro.Scan(&datarow.Telepon, &datarow.Name)
+		if errScan != nil {
+			log.Fatal("error scan select", errScan.Error())
+		}
+		User = append(User, datarow)
+
+	}
+
+	for _, v := range User {
+		// fmt.Println(v.Name, "    |", v.Telepon)
+		fmt.Println("Nama          :", v.Name)
+		fmt.Println("Nomor Telepon :", v.Telepon)
+	}
+}
